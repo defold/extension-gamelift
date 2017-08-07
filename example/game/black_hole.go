1e9af07d4af6aaf7df6d88882a383d08f752781e
Björn Ritzl <bjorn.ritzl@king.com>
2017-08-07 15:06:20 +0200

    Added black hole, chaser and roamer

--- example/game/black_hole.go
+++ example/game/black_hole.go
@@ -1,3 +1,18 @@
+components {
+  id: "black_hole"
+  component: "/example/game/black_hole.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
